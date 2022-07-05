bbd7fe28fad3ea328b0f15cbba89618c5ee6362c
EoJin-Kim <linexy@naver.com>
2022-07-05 18:04:57 +0900

    Feat : 이름 검색 기능

--- app/src/main/java/com/ej/snackapp/fragment/PickSnackFragment.kt
+++ app/src/main/java/com/ej/snackapp/fragment/PickSnackFragment.kt
@@ -2,6 +2,8 @@ package com.ej.snackapp.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -63,6 +65,36 @@ class PickSnackFragment : Fragment() {
             inputMethodManager.hideSoftInputFromWindow(pickSnackFragmentBinding.nameInput.windowToken,0)
             pickSnackFragmentBinding.snackSwipe.isRefreshing = false
         }
+
+        pickSnackFragmentBinding.nameInput.setText("김어진")
+        val textChangeListener = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+
+                thread {
+                    filterUserSnackInfoList.clear()
+                    for (userinfo in act.UserSnackInfoList) {
+
+                        if (userinfo.name.contains(s.toString())) {
+                            Log.d("test","after : ${userinfo.name}")
+                            filterUserSnackInfoList.add(userinfo)
+                        }
+                    }
+                    act?.runOnUiThread {
+                        pickSnackFragmentBinding.pickSnackRecycler.adapter?.notifyDataSetChanged()
+                    }
+
+                }
+
+            }
+        }
+        pickSnackFragmentBinding.nameInput.addTextChangedListener(textChangeListener)
 
         return pickSnackFragmentBinding.root
 
